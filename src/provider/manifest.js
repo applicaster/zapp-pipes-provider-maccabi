85885db54de967f2cd5269cf4de34aae9ad5e636
Tom Slutsky <tom.slutsky@petangos.com>
2018-07-22 11:19:35 +0300

    update manifest
    add descriptions and updated version

--- src/provider/manifest.js
+++ src/provider/manifest.js
@@ -2,7 +2,15 @@ export const manifest = {
   handlers: ['players', 'news'],
   help: {
     players: {
-      description: 'retrieves a collection or a list of players'
+      description: 'retrieves a list of players'
+    },
+    news: {
+      description: 'retrieves a list of news by category',
+      params: {
+        newsType: 'id of news type to retrieve (defaults to 0: all categories)',
+        from: 'index of first news item to retrieve',
+        to: 'index of last news item to retrieve'
+      }
     }
   }
 };

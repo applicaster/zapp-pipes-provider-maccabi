ce8b3d9ea8ce60efa5a4c61ce3cf6664742e9b8c
Tom Slutsky <tom.slutsky@petangos.com>
2018-08-05 08:30:30 +0300

    add getBoard function

--- src/provider/handler/comands.js
+++ src/provider/handler/comands.js
@@ -1,9 +1,11 @@
 import getPlayers from './getPlayers';
 import getNews from './getNews';
 import getMatchBox from './getMatchbox';
+import getBoard from './getBoard';
 
 export const commands = {
   players: getPlayers,
   news: getNews,
-  matchBox: getMatchBox
+  matchBox: getMatchBox,
+  board: getBoard
 };
--- src/provider/handler/getMatchBox/euroleagueLivaData.js
+++ src/provider/handler/getMatchBox/euroleagueLivaData.js
@@ -1,7 +1,3 @@
-import mock from './euroResponse.json';
-
-
 export function getEuroleagueLivaData(game) {
-    console.log(mock);
     return game;
 }

102c99fcb7b1614c3594058b003eab5f10226e0b
Tom Slutsky <tom.slutsky@petangos.com>
2018-08-07 11:35:41 +0300

    fix compareTime function and add test

--- src/provider/handler/__tests__/utils.test.js
+++ src/provider/handler/__tests__/utils.test.js
@@ -1,4 +1,7 @@
-import { sliceWrap, compareTimes } from '../utils';
+import {
+  sliceWrap,
+  compareTimes
+} from '../utils';
 describe('sliceWrap', () => {
   it('should return a list of n items wrapping a validated element', () => {
     expect(sliceWrap([])).toEqual([]);
@@ -27,3 +30,28 @@ describe('sliceWrap', () => {
     expect(sliceWrap([1, 2, 3, 4, 5], 2, val => val === 1)).toEqual([1, 2]);
   });
 });
+
+
+describe('compareTimes', () => {
+  it("should return -1 if a is earlier then b", () => {
+    expect(compareTimes("2018/05/05 12:00:00 +02:00", "2018/05/06 12:00:00 +02:00")).toBe(-1);
+    expect(compareTimes("2018/05/05 12:00:00 +02:00", "2018/06/05 12:00:00 +02:00")).toBe(-1);
+    expect(compareTimes("2018/05/05 12:00:00 +02:00", "2019/05/05 12:00:00 +02:00")).toBe(-1);
+    expect(compareTimes("2018/05/05 12:00:00 +02:00", "2018/05/05 13:00:00 +02:00")).toBe(-1);
+    expect(compareTimes("2018/05/05 12:00:00 +02:00", "2018/05/05 12:01:00 +02:00")).toBe(-1);
+    expect(compareTimes("2018/05/05 12:00:00 +02:00", "2018/05/05 12:00:01 +02:00")).toBe(-1);
+    expect(compareTimes("2018/05/05 12:00:00 +02:00", "2018/05/05 12:00:00 +01:00")).toBe(-1);
+  })
+  it("should return 0 when times are equal", () => {
+    expect(compareTimes("2018/05/05 12:00:00 +02:00", "2018/05/05 12:00:00 +02:00")).toBe(0)
+  })
+  it("should return 1 if b is earlier then a", () => {
+    expect(compareTimes("2018/05/06 12:00:00 +02:00", "2018/05/05 12:00:00 +02:00")).toBe(1);
+    expect(compareTimes("2018/06/05 12:00:00 +02:00", "2018/05/05 12:00:00 +02:00")).toBe(1);
+    expect(compareTimes("2019/05/05 12:00:00 +02:00", "2018/05/05 12:00:00 +02:00")).toBe(1);
+    expect(compareTimes("2018/05/05 13:00:00 +02:00", "2018/05/05 12:00:00 +02:00")).toBe(1);
+    expect(compareTimes("2018/05/05 12:01:00 +02:00", "2018/05/05 12:00:00 +02:00")).toBe(1);
+    expect(compareTimes("2018/05/05 12:00:01 +02:00", "2018/05/05 12:00:00 +02:00")).toBe(1);
+    expect(compareTimes("2018/05/05 12:00:00 +01:00", "2018/05/05 12:00:00 +02:00")).toBe(1);
+  })
+})

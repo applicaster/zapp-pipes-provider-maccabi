4e02869a40c081e56b7a53f40809413c2d445832
Tom Slutsky <tom.slutsky@petangos.com>
2018-11-01 22:58:20 +0200

    fix eueoloeage live

--- src/provider/handler/getMatchBox/euroleagueLivaData.js
+++ src/provider/handler/getMatchBox/euroleagueLivaData.js
@@ -13,7 +13,7 @@ export async function getEuroleagueLivaData(game, ex_game_id) {
       games_response = await axios.get(
         `http://live.euroleague.net/service.ashx?method=gamesxml&user=${USER}&password=${PASSWORD}`
       );
-    const game = parseXML(games_response.data).games.game.find(
+    const parsedGame = parseXML(games_response.data).games.game.find(
       item =>
         item.team1._cdata.toLowerCase().includes('maccabi') ||
         item.team2._cdata.toLowerCase().includes('maccabi')
@@ -25,12 +25,12 @@ export async function getEuroleagueLivaData(game, ex_game_id) {
         ...game.extensions,
 
         currentQuarter:
-          Math.ceil(game.minute._text.split(':')[0] / 10) <= 4
-            ? Math.ceil(game.minute._text.split(':')[0] / 10)
+          Math.ceil(parsedGame.minute._text.split(':')[0] / 10) <= 4
+            ? Math.ceil(parsedGame.minute._text.split(':')[0] / 10)
             : 'OT',
-        currentQuarterTimeMinutes: game.minute._text.split(':')[0] % 10,
-        home_team_score: game.homepts._text,
-        away_team_score: game.awaypts._text
+        currentQuarterTimeMinutes: parsedGame.minute._text.split(':')[0] % 10,
+        home_team_score: parsedGame.homepts._text,
+        away_team_score: parsedGame.awaypts._text
       }
     });
   } catch (e) {
